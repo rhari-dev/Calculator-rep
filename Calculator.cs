809d6fa046484769024efb23f1944d40421421af
unknown <r.hari@students.amikom.ac.id>
2026-04-08 14:53:15 +0700

    Menambahkan main (tugas 3)

--- Calculator.cs
+++ Calculator.cs
@@ -2,7 +2,14 @@ class Calculator
 (
 	static void Main(string[] args)
 	{
+		int a = 10;
+		int b = 6;
 		
+		Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}", a, b, Penambahan(a,b));
+		Console.WriteLine("Hasil Pengurangan: {0} - {1} = {2]", a, b, pengurangan(a,b));
+		
+		Console.WriteLine("\nTekan sembarang key untuk keluar");
+		Console.ReadKey();
 	}
 	
 	static int Penambaham(int a, int b)
